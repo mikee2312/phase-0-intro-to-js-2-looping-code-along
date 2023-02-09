ca1e7951dccbe6075d9e6beebcc520c80fd5cbef
mikee2312 <124088547+mikee2312@users.noreply.github.com>
2023-02-09 15:20:20 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+let messages =[]
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++ ) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+
+    }
+    return messages
+}
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
